c7e9adba1aec1ec80c559460ffbda00a95f78917
AbdulRahman Alzoubi <82521955+Abood-zoubi@users.noreply.github.com>
2025-01-07 20:22:45 +0300

    Delete Python directory

--- Python/test2.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-img_location = "C:\Users\zoubi\Desktop\my stuff\data"
-filename = "333923286_767270178101525_2728013072840416972_n"
-
-img = cv2.imread(img_location + filename)
-
-cv2.imshow('Original Image', img)
-cv2.waitKey(0)
