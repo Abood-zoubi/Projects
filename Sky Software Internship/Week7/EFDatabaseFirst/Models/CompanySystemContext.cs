c0c695cb3a4a0032a99cd0c310586d1a1ea45039
AbdulRahman Alzoubi <82521955+Abood-zoubi@users.noreply.github.com>
2025-02-08 22:03:32 +0300

    Add files via upload

---
+++ Sky Software Internship/Week7/EFDatabaseFirst/Models/CompanySystemContext.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDatabaseFirst.Models;
+
+public partial class CompanySystemContext : DbContext
+{
+    public CompanySystemContext()
+    {
+    }
+
+    public CompanySystemContext(DbContextOptions<CompanySystemContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Salary> Salaries { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-O13CF61E\\SQLEXPRESS;Database=CompanySystem;TrustServerCertificate=True;Trusted_Connection=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.DepartmentId).HasName("PK__Departme__C2232422D18539B8");
+
+            entity.Property(e => e.DepartmentId)
+                .ValueGeneratedNever()
+                .HasColumnName("department_id");
+            entity.Property(e => e.DepartmentName)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("department_name");
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__Employee__C52E0BA8B798ACE5");
+
+            entity.Property(e => e.EmployeeId)
+                .ValueGeneratedNever()
+                .HasColumnName("employee_id");
+            entity.Property(e => e.DepartmentId).HasColumnName("department_id");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("first_name");
+            entity.Property(e => e.HireDate).HasColumnName("hire_date");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("last_name");
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.DepartmentId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("FK__Employees__depar__4BAC3F29");
+        });
+
+        modelBuilder.Entity<Salary>(entity =>
+        {
+            entity.HasKey(e => e.SalaryId).HasName("PK__Salaries__A3C71C514EC57787");
+
+            entity.Property(e => e.SalaryId)
+                .ValueGeneratedNever()
+                .HasColumnName("salary_id");
+            entity.Property(e => e.EmployeeId).HasColumnName("employee_id");
+            entity.Property(e => e.EndDate).HasColumnName("end_date");
+            entity.Property(e => e.Salary1)
+                .HasColumnType("money")
+                .HasColumnName("salary");
+            entity.Property(e => e.StartDate).HasColumnName("start_date");
+
+            entity.HasOne(d => d.Employee).WithMany(p => p.Salaries)
+                .HasForeignKey(d => d.EmployeeId)
+                .HasConstraintName("FK__Salaries__employ__4E88ABD4");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Sky Software Internship/Week7/EFDatabaseFirst/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFDatabaseFirst.Models;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace EFDatabaseFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var DbContext = new CompanySystemContext();
+
+            var employees = (from emp in DbContext.Employees
+                             select new
+                             {
+                                 emp.FirstName,
+                                 emp.LastName,
+                                 DepartmentName = emp.Department.DepartmentName,
+                                 emp.HireDate
+                             })
+                             .ToList();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("First Query");
+            Console.WriteLine("------------------------------------------------------------------------------------");
+            
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"Employee: {emp.FirstName} {emp.LastName}, Department: {emp.DepartmentName}, Hire Date: {emp.HireDate}");
+            }
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("------------------------------------------------------------------------------------");
+            Console.WriteLine("Second Query");
+            Console.WriteLine("------------------------------------------------------------------------------------");
+            
+
+            var departments = (from dep in DbContext.Departments
+                               select new
+                               {
+                                   dep.DepartmentName,
+                                   Employees = dep.Employees.Select(e => e.FirstName + " " + e.LastName).ToList()
+                               }).ToList();
+
+
+            foreach (var dep in departments)
+            {
+                Console.WriteLine($"Department: {dep.DepartmentName}");
+                if (dep.Employees != null)
+                {
+                    foreach (var emp in dep.Employees)
+                    {
+                        if (emp != null)
+                        {
+                            Console.WriteLine($"Employee: {emp}");
+                        }
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Employee: null");
+                }
+            }
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("------------------------------------------------------------------------------------");
+            Console.WriteLine("Third Query");
+            Console.WriteLine("------------------------------------------------------------------------------------");
+            
+
+            var departmentsWithMoreThanOneEmployee = (from dep in DbContext.Departments
+                                                      where dep.Employees.Count > 1 
+                                                      select new
+                                                       {
+                                                        dep.DepartmentName,
+                                                        EmployeeCount = dep.Employees.Count 
+                                                       })
+                                                    .ToList();
+
+            foreach (var dep in departmentsWithMoreThanOneEmployee)
+            {
+                Console.WriteLine($"Department: {dep.DepartmentName}, Employee Count: {dep.EmployeeCount}");
+            }
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("------------------------------------------------------------------------------------");
+            Console.WriteLine("Fourth Query");
+            Console.WriteLine("------------------------------------------------------------------------------------");
+            
+
+            var departmentsWithEmployeesAbove5000 = (from dep in DbContext.Departments
+                                                     let employeesAbove5000 = dep.Employees
+                                                     .Where(emp => emp.Salaries
+                                                     .Any(salary => salary.Salary1 > 5000))
+                                                     where employeesAbove5000.Count() > 1
+                                                     select new
+                                                     {
+                                                         dep.DepartmentName,
+                                                         EmployeeCount = employeesAbove5000.Count()
+                                                     })
+                                                     .ToList();
+
+            foreach (var dep in departmentsWithEmployeesAbove5000)
+            {
+                Console.WriteLine($"Department: {dep.DepartmentName}, Employee Count of Salary above 5000): {dep.EmployeeCount}");
+            }
+            Console.ResetColor();
+        }
+    }
+}
